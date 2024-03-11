ee25b600bb0fa65a0b2c4f111f00dd969c1cdf93
Aakash Singh <singh.aakash0515@gmail.com>
2024-03-11 12:48:03 +0530

    first change

---
+++ abc.java
@@ -0,0 +1,16 @@
+public class abc {
+    public static void main(String[] args) {
+        int n = 100;
+        int m = 200;
+        int sum = n + m;
+        int diff = m - n;
+        int prod = m * n;
+        int div = m / n;
+        int rem = m % n;
+        System.out.println("sum=" + sum);
+        System.out.println("diff=" + diff);
+        System.out.println("prod=" + prod);
+        System.out.println("div=" + div);
+        System.out.println("rem=" + rem);
+    }
+}
