481b827b58c1608623b6c47312615e043f03301f
Aakash Singh <singh.aakash0515@gmail.com>
2024-03-11 13:08:46 +0530

    3rd change

---
+++ def.java
@@ -0,0 +1,12 @@
+public class def {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 2;
+        int pow = (int) Math.pow(a, b);
+        int min = Math.min(a, b);
+        System.out.println("pow=" + pow);
+        System.out.println("min=" + min);
+    }
+
+}
